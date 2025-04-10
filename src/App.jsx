3a7ea5be56a9e0db7ecaf61093293b9479b02590
Channarong Chancher <143964932+lovemxchine@users.noreply.github.com>
2025-04-10 21:13:15 +0700

    initial setup backoffice

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="border rounded-full border-gray-200 size-20 bg-red-500"></div>
+    </>
+  );
+}
+
+export default App;
